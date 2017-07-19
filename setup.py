98124b64682342b78a92c472411aa630035ab1c5
mingfei <haomingfei.marvin@gmail.com>
2017-07-18 23:29:19 -0400

    init project

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup, Extension
+from distutils.command.clean import clean
+from distutils.command.install import install
+import os
+import numpy as np
+
+
+class RsInstall(install):
+    def run(self):
+        install.run(self)
+        c = clean(self.distribution)
+        c.all = True
+        c.finalize_options()
+        c.run()
+
+NUMPY_PATH = np.get_include()
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+LOCAL_INCLUDE = os.path.join(DIR_PATH, 'include')
+
+setup(
+    name='_pyrs',
+    version='1.0',
+    cmdclass={'install': RsInstall},
+    ext_modules=[
+        Extension(
+            '_pyrs', [os.path.join(DIR_PATH, '_pyrs.c')],
+            libraries=['realsense'],
+            include_dirs=[NUMPY_PATH, '/usr/local/include', LOCAL_INCLUDE],
+            library_dirs=['/usr/local/lib'],
+            extra_compile_args=['-g']
+        ),
+    ],
+)
